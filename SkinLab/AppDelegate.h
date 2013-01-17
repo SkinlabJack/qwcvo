96a82023ab1b3fcb22276e856c4c05ed1b5325e5
Dai Qinfu <thewingofthesky@QfmatoMacBook-Pro.local>
2013-01-17 11:08:05 +0800

    Initial Commit

---
+++ SkinLab/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SkinLab
+//
+//  Created by Dai Qinfu on 13-1-17.
+//  Copyright (c) 2013年 北京思然加互联网科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SkinLab/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SkinLab
+//
+//  Created by Dai Qinfu on 13-1-17.
+//  Copyright (c) 2013年 北京思然加互联网科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
